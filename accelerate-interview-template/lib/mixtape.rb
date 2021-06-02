0a3fa27b55faf74d749342bb2e87900234675695
= <connor20c@gmail.com>
2021-06-01 17:13:50 -0700

    completed

--- accelerate-interview-template/lib/mixtape.rb
+++ accelerate-interview-template/lib/mixtape.rb
@@ -2,6 +2,34 @@
 
 module Mixtape
   def self.apply_changes(mixtape, changes)
-    raise NotImplementedError, 'Your code goes here!'
+    changes.each do |child|
+      if child[:type].eql? "add_playlist"
+        newObj = child
+        newObj.merge!({:id => (mixtape[:playlists].size + 1).to_s, :owner_id => child[:user_id]})
+        newObj.delete(:type)
+        newObj.delete(:user_id)
+        mixtape[:playlists].push(newObj)
+      elsif child[:type].eql? "remove_playlist"
+        if !mixtape[:playlists].any? {|playlist| playlist[:id] == child[:playlist_id] }
+          raise "playlist does not exist"
+        end
+        mixtape[:playlists] = mixtape[:playlists].reject {|hash| hash[:id].eql? child[:playlist_id]}
+
+      elsif child[:type].eql? "add_song_to_playlist"
+        if !mixtape[:playlists].any? {|playlist| playlist[:id] == child[:playlist_id] }
+          raise "playlist does not exist"
+        end
+        if !mixtape[:songs].any? {|song| song[:id] == child[:song_id] }
+          raise "song does not exist"
+        end
+        index = mixtape[:playlists].index{ |list| list[:id].eql? child[:playlist_id]}
+        if mixtape[:playlists].fetch(index)[:song_ids].include? child[:song_id]
+          raise "song already added to playlist"
+        end
+        mixtape[:playlists].fetch(index)[:song_ids].push(child[:song_id])
+
+      end
+    end
+    return mixtape
   end
 end
--- accelerate-interview-template/spec/mixtape/cli_spec.rb
+++ accelerate-interview-template/spec/mixtape/cli_spec.rb
@@ -5,7 +5,7 @@ require 'tempfile'
 require 'mixtape/cli'
 
 RSpec.describe Mixtape::CLI do
-  describe '.apply_changes' do
+  describe '.add_playlist' do
     let(:input) do
       {
         users: [{ id: '1', name: 'Albin Jaye' }],
@@ -19,7 +19,7 @@ RSpec.describe Mixtape::CLI do
     let(:output) do
       {
         **input,
-        playlists: [{ id: '1', user_id: '1', song_ids: ['1'] }]
+        playlists: [{ id: '1', owner_id: '1', song_ids: ['1'] }]
       }
     end
 
@@ -32,9 +32,206 @@ RSpec.describe Mixtape::CLI do
     let(:output_file) { json_tempfile('output') }
     let(:files) { [input_file, changes_file, output_file] }
 
-    let!(:mixtape_class) do
-      class_double('Mixtape').as_stubbed_const(transfer_nested_constants: true)
+    before do
+      input_file.write(input.to_json)
+      changes_file.write(changes.to_json)
     end
+
+    after do
+      files.each do |file|
+        file.close
+        file.unlink
+      end
+    end
+
+    it 'adds the playlist' do
+      
+      described_class.apply_changes(files.map(&:path))
+
+      expect(described_class.read_json(output_file)).to eq(output)
+    end
+  end
+  describe '.remove_playlist' do
+    let(:input) do
+      {
+        users: [{ id: '1', name: 'Albin Jaye' }],
+        playlists: [{ id: '1', owner_id: '1', song_ids: ['1'] }, { id: '2', user_id: '1', song_ids: ['1'] }],
+        songs: [
+          { id: '1', artist: 'Camila Cabello', title: 'Never Be the Same' }
+        ]
+      }
+    end
+    let(:input2) do
+      {
+        users: [{ id: '1', name: 'Albin Jaye' }],
+        playlists: [{ id: '1', owner_id: '1', song_ids: ['1'] }],
+        songs: [
+          { id: '1', artist: 'Camila Cabello', title: 'Never Be the Same' }
+        ]
+      }
+    end
+    let(:changes) { [{ type: 'remove_playlist', playlist_id: '2'}] }
+    let(:output) do
+      {
+        users: [{ id: '1', name: 'Albin Jaye' }],
+        playlists: [{ id: '1', owner_id: '1', song_ids: ['1'] }],
+        songs: [
+          { id: '1', artist: 'Camila Cabello', title: 'Never Be the Same' }
+        ]
+      }
+    end
+
+    def json_tempfile(basename)
+      Tempfile.new([basename, '.json']).tap { |file| file.sync = true }
+    end
+
+    let(:input_file) { json_tempfile('input') }
+    let(:input_file2) { json_tempfile('input2') }
+    let(:changes_file) { json_tempfile('changes') }
+    let(:output_file) { json_tempfile('output') }
+    let(:files) { [input_file, changes_file, output_file] }
+    let(:files2) { [input_file2, changes_file, output_file] }
+
+    before do
+      input_file.write(input.to_json)
+      input_file2.write(input2.to_json)
+      changes_file.write(changes.to_json)
+    end
+
+    after do
+      files.each do |file|
+        file.close
+        file.unlink
+      end
+      files2.each do |file|
+        file.close
+        file.unlink
+      end
+    end
+
+    it 'removes a playlist' do
+      
+      described_class.apply_changes(files.map(&:path))
+
+      expect(described_class.read_json(output_file)).to eq(output)
+    end
+
+    it 'raises exception if playlist does not exist' do
+      expect do
+        described_class.apply_changes(files2.map(&:path))
+      end.to raise_error("playlist does not exist")
+    end
+  end
+  describe '.add_song_to_playlist' do
+    let(:input) do
+      {
+        users: [{ id: '1', name: 'Albin Jaye' }],
+        playlists: [{ id: '1', owner_id: '1', song_ids: ['1'] }],
+        songs: [
+          { id: '1', artist: 'Camila Cabello', title: 'Never Be the Same' }, { "id": "2",
+            "artist": "Zedd", "title": "The Middle" },
+        ]
+      }
+    end
+    let(:changes) { [{ type: 'add_song_to_playlist', playlist_id: '1', song_id: '2'}] }
+    let(:changes2) { [{ type: 'add_song_to_playlist', playlist_id: '3', song_id: '2'}] }
+    let(:changes3) { [{ type: 'add_song_to_playlist', playlist_id: '1', song_id: '3'}] }
+    let(:changes4) { [{ type: 'add_song_to_playlist', playlist_id: '1', song_id: '1'}] }
+    let(:output) do
+      {
+        users: [{ id: '1', name: 'Albin Jaye' }],
+        playlists: [{ id: '1', owner_id: '1', song_ids: ['1', '2'] }],
+        songs: [
+          { id: '1', artist: 'Camila Cabello', title: 'Never Be the Same' }, { "id": "2",
+            "artist": "Zedd", "title": "The Middle" },
+        ]
+      }
+    end
+
+    def json_tempfile(basename)
+      Tempfile.new([basename, '.json']).tap { |file| file.sync = true }
+    end
+
+    let(:input_file) { json_tempfile('input') }
+    let(:changes_file) { json_tempfile('changes') }
+    let(:changes_file2) { json_tempfile('changes2') }
+    let(:changes_file3) { json_tempfile('changes3') }
+    let(:changes_file4) { json_tempfile('changes4') }
+    let(:output_file) { json_tempfile('output') }
+    let(:files) { [input_file, changes_file, output_file] }
+    let(:files2) { [input_file, changes_file2, output_file] }
+    let(:files3) { [input_file, changes_file3, output_file] }
+    let(:files4) { [input_file, changes_file4, output_file] }
+
+    before do
+      input_file.write(input.to_json)
+      changes_file.write(changes.to_json)
+      changes_file2.write(changes2.to_json)
+      changes_file3.write(changes3.to_json)
+      changes_file4.write(changes4.to_json)
+    end
+
+    after do
+      files.each do |file|
+        file.close
+        file.unlink
+      end
+      files2.each do |file|
+        file.close
+        file.unlink
+      end
+      files3.each do |file|
+        file.close
+        file.unlink
+      end
+      files4.each do |file|
+        file.close
+        file.unlink
+      end
+    end
+
+    it 'adds a song to the playlist' do
+      
+      described_class.apply_changes(files.map(&:path))
+
+      expect(described_class.read_json(output_file)).to eq(output)
+    end
+
+    it 'raises exception if playlist does not exist' do
+      expect do
+        described_class.apply_changes(files2.map(&:path))
+      end.to raise_error("playlist does not exist")
+    end
+    it 'raises exception if song does not exist' do
+      expect do
+        described_class.apply_changes(files3.map(&:path))
+      end.to raise_error("song does not exist")
+    end
+    it 'raises exception if song already added to playlist' do
+      expect do
+        described_class.apply_changes(files4.map(&:path))
+      end.to raise_error("song already added to playlist")
+    end
+  end
+  describe '.test_all' do
+    let(:input) do
+      JSON.parse(File.read('example/mixtape.json'))
+    end
+    let(:changes) do
+      JSON.parse(File.read('example/changes.json'))
+    end
+    let(:output) do
+      JSON.parse(File.read('example/output.json'), :symbolize_names => true)
+    end
+
+    def json_tempfile(basename)
+      Tempfile.new([basename, '.json']).tap { |file| file.sync = true }
+    end
+
+    let(:input_file) { json_tempfile('input') }
+    let(:changes_file) { json_tempfile('changes') }
+    let(:output_file) { json_tempfile('output') }
+    let(:files) { [input_file, changes_file, output_file] }
 
     before do
       input_file.write(input.to_json)
@@ -60,11 +257,7 @@ RSpec.describe Mixtape::CLI do
       end.to raise_error(described_class::Error, /got 4/)
     end
 
-    it 'writes changes to the output' do
-      allow(mixtape_class).to receive(:apply_changes)
-        .with(input, changes)
-        .and_return(output)
-
+    it 'applies all changes' do
       described_class.apply_changes(files.map(&:path))
 
       expect(described_class.read_json(output_file)).to eq(output)
